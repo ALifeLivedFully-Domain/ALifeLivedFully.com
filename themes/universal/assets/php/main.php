f26d824d50f74de2abf35ff2ff1f97387ba9ca34
Dani <96098717+ALifeLivedFully@users.noreply.github.com>
2024-08-03 14:30:26 +0000

    Update main.php

--- themes/universal/assets/php/main.php
+++ themes/universal/assets/php/main.php
@@ -2,7 +2,7 @@
     <body>
         <h1> Testing PHP </h1>
         <?php
-            echo "The current time is " . date("h:i:sa");
+            echo "The time is " . date("h:i:sa");
         ?>
     </body>
 </html>
