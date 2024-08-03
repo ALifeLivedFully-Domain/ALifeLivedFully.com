a3ede5388289b7d9dd7a9e4b8b389d6a5471bbd6
Dani <Dani@ALifeLivedFully.com>
2024-08-03 14:52:12 -0500

    remove assets/php and other php stuff

--- content/test.md
+++ content/test.md
@@ -3,4 +3,4 @@ title: Test
 date: 2024-07-19T19:29:23-05:00
 draft: false
 ---
-Hello world
+Hello world.
--- public/php/main.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <h1> Testing PHP </h1>
-        <?php
-            echo "Hello world";
-            echo "testinggg";
-        ?>
-    </body>
-</html>
