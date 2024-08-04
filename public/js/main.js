c30dfc80d65a7dc57248d0b77e6135aff9761f87
Dani <Dani@ALifeLivedFully.com>
2024-08-04 14:28:20 -0500

    start of a javascript free responsive hamburger menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,4 @@
 (() => {
   // <stdin>
   console.log("Hello world! This site was generated using Hugo by alifelivedfully.com");
-  var primaryNav = document.querySelector(".primary-navigation");
-  var navToggle = document.querySelector(".mobile-nav-toggle");
-  navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute("data-visible");
-    console.log(visibility);
-  });
 })();
